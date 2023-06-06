44d75adb9c8110a1ec2a66c39d65a46e0c93788a
han <walcoorperation@gmail.com>
2023-06-07 01:14:11 +0800

    Add module federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+plugins: [
+    react(),
+    federation({
+      name: 'app',
+      remotes: {
+        remoteApp: 'https://profiler.walcron.com/assets/remoteEntry.js'
+      },
+      shared: ['react','react-dom']
+    })
+  ],
+  build: {
+    target: 'esnext',
+  }
 })

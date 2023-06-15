a85fafaa1c85996cd4580cf1bb24f3f00a0b9926
han <walcoorperation@gmail.com>
2023-06-16 07:39:09 +0800

    Update to add test

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-plugins: [
+  plugins: [
     react(),
     federation({
-      name: 'app',
+      name: "app",
       remotes: {
-        remoteApp: 'https://profiler.walcron.com/assets/remoteEntry.js'
+        remoteApp: "https://profiler.walcron.com/assets/remoteEntry.js",
       },
-      shared: ['react','react-dom']
-    })
+      shared: ["react", "react-dom"],
+    }),
   ],
   build: {
     modulePreload: false,
-    target: 'esnext',
+    target: "esnext",
     minify: false,
-    cssCodeSplit: false
-  }
-})
+    cssCodeSplit: false,
+  },
+  test: {
+    global: true,
+    environment: "jsdom",
+    setupFiles: "./test-setup.js",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "cobertura"],
+      threshold: {
+        global: {
+          branches: 100,
+          functions: 100,
+          lines: 100,
+          statements: 100,
+        },
+      },
+    },
+  },
+});

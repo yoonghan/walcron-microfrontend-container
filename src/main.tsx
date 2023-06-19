e6f7b253a170c695a2d4ba30041f73a14f898146
han <walcoorperation@gmail.com>
2023-06-19 13:57:46 +0800

    Add a readme.

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,6 @@
+/**
+ * It is not necessary to implement bootstrap as it's handled by vite.
+ * But this import ensure it runs asynchronously (notice it is a function and not a synch import "./bootstrap") and made portable for other frameworks.
+ */
+
 import("./bootstrap");

77b3b2cbc0b00a301b221e52bd42f82538beced2
han <walcoorperation@gmail.com>
2023-06-21 04:41:01 +0800

    Add test cases for route and error page.

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./routes/Root";
-import ErrorPage from "./error-page";
+import ErrorPage from "./ErrorPage";
 import appRoutes from "remoteApp/appRoutes";
 
-export default createBrowserRouter([
+export const definedRoute = [
+  {
+    path: "/profiler",
+    element: <Root />,
+    children: [...appRoutes],
+  },
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [...appRoutes],
   },
-]);
+];
+
+/* istanbul ignore next */
+export default createBrowserRouter(definedRoute);

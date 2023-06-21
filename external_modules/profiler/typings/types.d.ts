b099bc5126476f88cb1c38763cc9d7043fdc9a26
walcron-github-action <walcoorperation@gmail.com>
2023-06-21 06:59:00 +0000

    Generate new profiler dist

---
+++ external_modules/profiler/typings/types.d.ts
@@ -0,0 +1,41 @@
+
+    declare module "profiler/SignIn" {
+      export default function SignIn(): import("react/jsx-runtime").JSX.Element;
+
+    }
+    
+    declare module "profiler/Button" {
+      const Button: () => import("react/jsx-runtime").JSX.Element;
+export default Button;
+
+    }
+    
+    declare module "profiler/GroupButton" {
+      interface Props {
+    buttons: {
+        text: string;
+    }[];
+}
+const GroupButton: ({ buttons }: Props) => import("react/jsx-runtime").JSX.Element;
+export default GroupButton;
+
+    }
+    
+    declare module "profiler/appRoutes" {
+      import { RouteObject } from "react-router-dom";
+const routes: RouteObject[];
+export default routes;
+
+    }
+    
+    declare module "profiler/Contact" {
+      export default function Contact(): import("react/jsx-runtime").JSX.Element;
+
+    }
+    
+    declare module "profiler/Basic" {
+      const _default: () => import("react/jsx-runtime").JSX.Element;
+export default _default;
+
+    }
+    

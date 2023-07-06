181313882d42d23728d94656f3ed3bf21f23d0c9
han <walcoorperation@gmail.com>
2023-07-06 13:43:35 +0800

    Add navigation listener

--- src/components/Profiler/index.tsx
+++ src/components/Profiler/index.tsx
@@ -1,33 +1,72 @@
 import {
-  BrowserRouter,
+  Routes,
   Link,
-  Router,
-  RouterProvider,
-  createMemoryRouter,
+  MemoryRouter,
+  Route,
   useLocation,
   useNavigate,
+  createMemoryRouter,
+  RouterProvider,
 } from "react-router-dom";
-import appRoute from "profiler/appRoutes";
-import ErrorPage from "../../routes/ErrorPage";
-import { useCallback, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as ReactDOM from "react-dom/client";
 import { AuthenticationContext } from "../../context/authentication";
+import appRoute from "profiler/appRoutes";
+import ErrorPage from "../../routes/ErrorPage";
 
 type Props = {
   onSignIn: () => void;
   onSignOut: () => void;
 };
 
+const baseUrl = "/profiler";
+
 const Profiler = ({ onSignIn, onSignOut }: Props) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
   const rootRef = useRef<ReactDOM.Root>();
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
+    if (location.pathname.startsWith(baseUrl)) {
+      window.dispatchEvent(
+        new CustomEvent("[container] navigated", {
+          detail: location.pathname.replace(baseUrl, ""),
+        })
+      );
+    }
+  }, [location]);
+
+  useEffect(() => {
+    const app2NavigationEventHandler = (event: Event) => {
+      const pathname = (event as CustomEvent<string>).detail;
+      const newPathname = `${baseUrl}${pathname === "/" ? "" : pathname}`;
+      console.log(`${newPathname} vs ${location.pathname}`);
+      if (newPathname === location.pathname) {
+        return;
+      }
+      navigate(newPathname);
+    };
+    window.addEventListener("[profiler] navigated", app2NavigationEventHandler);
+
+    return () => {
+      window.removeEventListener(
+        "[profiler] navigated",
+        app2NavigationEventHandler
+      );
+    };
+  }, [location, navigate]);
+
+  useEffect(() => {
+    // queueMicrotask(() => {
+    if (rootRef.current) {
+      return;
+    }
+
     const router = createMemoryRouter(
       [
         {
-          ...appRoute("/profiler", <ErrorPage />, {
+          ...appRoute("/profiler", "profiler", "container", <ErrorPage />, {
             onSignIn,
             onSignOut,
           }),
@@ -36,17 +75,16 @@ const Profiler = ({ onSignIn, onSignOut }: Props) => {
       { initialEntries: [location.pathname] }
     );
 
-    queueMicrotask(() => {
-      rootRef.current = ReactDOM.createRoot(wrapperRef.current!);
-      rootRef.current.render(<RouterProvider router={router} />);
-    });
+    rootRef.current = ReactDOM.createRoot(wrapperRef.current!);
+    rootRef.current.render(<RouterProvider router={router} />);
+    // });
 
-    return () => {
-      queueMicrotask(() => {
-        rootRef.current?.unmount();
-        rootRef.current = undefined;
-      });
-    };
+    // return () => {
+    //   queueMicrotask(() => {
+    //     rootRef.current?.unmount();
+    //     rootRef.current = undefined;
+    //   });
+    // };
   }, [location.pathname, onSignIn, onSignOut]);
 
   return <div ref={wrapperRef} />;

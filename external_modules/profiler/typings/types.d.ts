f61b7380b01204480fc2f15dcced35e33ed804c3
walcron-github-action <walcoorperation@gmail.com>
2023-07-21 09:14:24 +0000

    Generate new profiler dist

--- external_modules/profiler/typings/types.d.ts
+++ external_modules/profiler/typings/types.d.ts
@@ -1,47 +1,18 @@
 
-    declare module "profiler/SignIn" {
-      export default function SignIn({ onSignIn }: {
-    onSignIn: () => void;
-}): import("react/jsx-runtime").JSX.Element;
-
-    }
-    
-    declare module "profiler/Button" {
-      const Button: () => import("react/jsx-runtime").JSX.Element;
-export default Button;
-
-    }
-    
-    declare module "profiler/GroupButton" {
-      interface Props {
-    buttons: {
-        text: string;
-    }[];
-}
-const GroupButton: ({ buttons }: Props) => import("react/jsx-runtime").JSX.Element;
-export default GroupButton;
-
-    }
-    
     declare module "profiler/appRoutes" {
-      import { RouteObject } from "react-router-dom";
+      import { ReactNode } from "react";
+import { RouteObject } from "react-router-dom";
 type Props = {
     onSignIn: () => void;
     onSignOut: () => void;
 };
-const routes: (appName?: string, containerName?: string, errorElement?: import("react/jsx-runtime").JSX.Element, props?: Props) => RouteObject;
+const routes: ({ appName, containerName, errorElement, props, }: {
+    appName?: string | undefined;
+    containerName?: string | undefined;
+    errorElement?: ReactNode;
+    props: Props;
+}) => RouteObject;
 export default routes;
-
-    }
-    
-    declare module "profiler/Contact" {
-      export default function Contact(): import("react/jsx-runtime").JSX.Element;
-
-    }
-    
-    declare module "profiler/Basic" {
-      const _default: () => import("react/jsx-runtime").JSX.Element;
-export default _default;
 
     }
     

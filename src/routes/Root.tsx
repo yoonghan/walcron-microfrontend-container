c31664cba844514c27d8df54ff88ca748cbc1c09
han <walcoorperation@gmail.com>
2023-07-31 12:13:48 +0800

    Code cleanup

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,6 +1,4 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { Link } from "@mui/material";
-import { chartPath, profilerPath } from "./constants";
 import Header from "../components/Header";
 import NavigationBreadcrumb from "../components/NavigationBreadcrumb";
 export default function Root() {

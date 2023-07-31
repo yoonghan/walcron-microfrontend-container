99003aa61d743b34c01af4df270a91dd4ca7230a
han <walcoorperation@gmail.com>
2023-07-31 10:44:06 +0800

    Add main page.

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -2,45 +2,11 @@ import { Outlet } from "react-router-dom";
 import { Link } from "@mui/material";
 import { chartPath, profilerPath } from "./constants";
 import Header from "../components/Header";
-
 export default function Root() {
   return (
     <>
       <Header />
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link href={`${profilerPath}`} color="inherit">
-                Profiler
-              </Link>
-            </li>
-            <li>
-              <Link href={`${chartPath}`} color="inherit">
-                Chart
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div id="detail">
+      <div id="outlet" data-testid="outlet">
         <Outlet />
       </div>
     </>
